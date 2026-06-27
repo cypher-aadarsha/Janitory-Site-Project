50038caa45481241fd1d6abcad12fb4df0091e18
Aadarsha Jha <153203025+Er7890@users.noreply.github.com>
2026-06-27 21:08:19 +0545

    Fix CSP policy

--- janitorial_site/middleware.py
+++ janitorial_site/middleware.py
@@ -55,8 +55,8 @@ class ContentSecurityPolicyMiddleware:
         # Content-Security-Policy: Allow our own resources + trusted CDNs
         csp = (
             "default-src 'self'; "
-            "script-src 'self' 'unsafe-inline' https://unpkg.com https://cdnjs.cloudflare.com https://www.googletagmanager.com https://www.google-analytics.com; "
-            "style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.googleapis.com; "
+            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com https://cdnjs.cloudflare.com https://www.googletagmanager.com https://www.google-analytics.com https://static.cloudflareinsights.com; "
+            "style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.googleapis.com https://unpkg.com; "
             "font-src 'self' https://cdnjs.cloudflare.com https://fonts.gstatic.com; "
             "img-src 'self' data: https:; "
             "connect-src 'self' https://www.google-analytics.com; "
